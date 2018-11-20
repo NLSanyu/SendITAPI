3d4cc701671de159b471ee979ac0258b5ca922c5
NLSanyu <lydiansanyu@gmail.com>
2018-11-20 22:47:23 +0200

    Edit tests.

--- test/test_users.py
+++ test/test_users.py
@@ -5,6 +5,9 @@ import pytest
 from app import app
 
 class APITestUsers(unittest.TestCase):
+
+	token = ''
+
 	def setUp(self):
 		self.app = app
 		self.client = self.app.test_client()
@@ -12,23 +15,25 @@ class APITestUsers(unittest.TestCase):
 	def test_signup(self):
 		user = {"username": "lydia", "email": "lydia@gmail.com", "password": "pass123"}
 		response = self.client.post('/api/v1/auth/signup', json=user)
+		token = response['access_token']
 		self.assertEqual(response.status_code, 201)
 		#self.assertIn(str(response), "user created")
 
 	def test_login(self):
-		user = {"username": "lydia", "email": "lydia@gmail.com", "password": "pass123"}
+		user = {"username": "lydia", "password": "pass123"}
 		response = self.client.post('/api/v1/auth/login', json=user)
-		self.assertEqual(response.status_code, 201)
+		token = response['access_token']
+		self.assertEqual(response.status_code, 200)
 		#self.assertIn(str(response), "user created")
 
-	def test_fetch_users(self):
+	def test_get_users(self):
 		"""
 			Test for fetching all users
 		"""
-		response = self.client.get('/api/v1/users', content_type='application/json')
+		response = self.client.get('/api/v1/users', content_type='application/json', authorization=token)
 		self.assertEqual(response.status_code, 200)
 
-	def test_fetch_user_parcels(self):
+	def test_get_user_parcels(self):
 		"""
 			Test for fetching all parcels of a specific user
 		"""
