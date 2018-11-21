eb3031bcf327b75c333315a7ef70e24924516155
NLSanyu <lydiansanyu@gmail.com>
2018-11-21 14:13:50 +0200

    Edit admin users routes

--- app/api/admin.py
+++ app/api/admin.py
@@ -12,10 +12,10 @@ def change_parcel_status(parcel_id):
 	"""
 		Function for API endpoint to change the status a parcel delivery order
 	"""
-	all_status = ["Delivered", "Cancel", "New", "In Transit", "Not picked up"]
+	all_status = ["Delivered", "Cancelled", "New", "In Transit", "Not picked up"]
 	req = request.json
 	if 'status' not in req.keys():
-		return jsonify({'message':'no status entered', 'status':'failure'}), 400
+		return jsonify({'message': 'no status entered', 'status' :'failure'}), 400
 
 	status = request.json['status']
 	if status in all_status:
@@ -24,22 +24,22 @@ def change_parcel_status(parcel_id):
 		db.cur.execute(query, (parcel_id,))
 		db.connection.commit()
 		result = db.cur.fetchall()
-		if result != None:
-			for row in db.cur:
+		if result:
+			for row in result:
 				if row[8] == "Delivered":
-					return jsonify({'message':'parcel already delivered', 'status':'failure'}), 400
+					return jsonify({'message': 'parcel already delivered', 'status': 'failure'}), 400
 				else:
 					query = """UPDATE parcels SET status = %s WHERE id = %s"""
 					db.cur.execute(query, (status, parcel_id, ))
 					db.cur.close()
 					db.connection.close()
-					return jsonify({'message':'parcel status updated', 'status':'success'}), 400	
+					return jsonify({'message': 'parcel status updated', 'status' :'success'}), 400	
 		else: 	
 			db.cur.close()
 			db.connection.close()
-			return jsonify({'message':'parcel non-existent', 'status':'failure'}), 400
+			return jsonify({'message': 'parcel non-existent', 'status': 'failure'}), 400
 	else:
-		return jsonify({'message':'incorrect status', 'status':'failure'}), 400	
+		return jsonify({'message': 'incorrect status', 'status': 'failure'}), 400	
 
 
 @app.route('/api/v1/parcels/<int:parcel_id>/presentLocation', methods=['PUT'])
@@ -49,9 +49,9 @@ def change_parcel_location(parcel_id):
 	"""
 	req = request.json
 	if 'location' in req.keys():
-		location = request.json['present_location']
+		location = request.json['location']
 		if location == "": 
-			return jsonify({'message':'present location is empty', 'status':'failure'}), 400
+			return jsonify({'message': 'present location is empty', 'status': 'failure'}), 400
 		else:
 			if len(location) > 124:
 				return jsonify({'message':'present location should not be longer than 124 characters', 'status':'failure'}), 400
@@ -61,18 +61,19 @@ def change_parcel_location(parcel_id):
 		
 		db.cur.execute(query, (parcel_id,))
 		db.connection.commit()
-		if db.cur.fetchall:
-			for row in db.cur:
-				if row[9] == "Delivered":
-					return jsonify({'message':'parcel already delivered', 'status':'failure'}), 400
+		result = db.cur.fetchall()
+		if result:
+			for row in result:
+				if row[8] == "Delivered":
+					return jsonify({'message': 'parcel already delivered', 'status': 'failure'}), 400
 				else:
 					query = """UPDATE parcels SET present_location = %s WHERE id = %s"""
 					db.cur.execute(query, (location, parcel_id, ))	
 					return jsonify({'message':'parcel present location updated', 'status':'success'}), 400
 		else: 
-			return jsonify({'message':'parcel non-existent', 'status':'failure'}), 400	
+			return jsonify({'message': 'parcel non-existent', 'status': 'failure'}), 400	
 	else:
-		return jsonify({'message':'no location entered', 'status':'failure'}), 400	
+		return jsonify({'message': 'no location entered', 'status': 'failure'}), 400	
 
 
 def connect_to_db():
