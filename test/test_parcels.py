45125e7f4b3669b89c3e90162d78238663d522d8
NLSanyu <lydiansanyu@gmail.com>
2018-11-21 08:27:11 +0200

    Edit tests for get parcels route

--- test/test_parcels.py
+++ test/test_parcels.py
@@ -2,12 +2,23 @@ import unittest
 import os
 import json
 import pytest
+from test.test_users import APITestUsers
 from app import app
+
+user = APITestUsers()
 
 class APITest(unittest.TestCase):
 	def setUp(self):
 		self.app = app
 		self.client = self.app.test_client()
+
+	def get_token(self):
+		"""
+			Function for getting an access token
+		"""
+		response = self.client.post('/api/v1/auth/login', json=user.login_user)
+		access_token = response.json['access_token']
+		return access_token
 
 	def test_home(self):
 		"""
@@ -16,7 +27,13 @@ class APITest(unittest.TestCase):
 		response = self.client.get('/api/v1')
 		self.assertEqual(response.status_code, 200)
 
-	
+	def test_get_parcels(self):
+		"""
+			Test for fetching all parcels
+		"""
+		token = self.get_token()
+		response = self.client.get('/api/v1/parcels', content_type='application/json', headers={'Authorization': f'Bearer {token}'})
+		self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
