4530901d283dc75b5f33b23228b29e4185e49116
NLSanyu <lydiansanyu@gmail.com>
2018-12-03 16:45:11 +0200

    Refactor user helper functions

--- app/api/helpers/user_helpers.py
+++ app/api/helpers/user_helpers.py
@@ -5,7 +5,7 @@ def validate_email(email):
 	pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 	return re.match(pattern, email)
 
-def convert_user_to_dict(result):
+def convert_users_to_dict(result):
 	data_list = []
 	for row in result:
 		data = dict()
@@ -19,5 +19,19 @@ def convert_user_to_dict(result):
 		data['in_transit'] = row[7]
 		data_list.append(data)
 
+	return data_list
+
+def convert_one_user_to_dict(result):
+	data_list = []
+	for row in result:
+		data = dict()
+		data['username'] = row[1]
+		data['email'] = row[2]
+		data['phone_number'] = row[3]
+		data['orders'] = row[5]
+		data['delivered'] = row[6]
+		data['in_transit'] = row[7]
+		data_list.append(data)
+		
 	return data_list
 
