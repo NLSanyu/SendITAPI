7aa1074c310541663be4233d13ec5e32394d802f
NLSanyu <lydiansanyu@gmail.com>
2018-12-07 12:05:35 +0200

    Sort parcels and users in descending order in admin routes

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,20 @@ from flask_cors import CORS
 import flask_jwt_extended
 import flasgger
 from flask_jwt_extended import JWTManager
+from app.models.models import Tables
+from app.api import users, parcels, admin 
+
+tables = Tables()
+tables.create_tables()
 
 app = Flask(__name__)
+tables.create_tables()
 app.config.from_object(Config)
 flasgger.Swagger(app)
 app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
 jwt = JWTManager(app)
 CORS(app)
 
-from app.models.models import Tables 
-tables = Tables()
-tables.create_tables()
-from app.api import users, parcels, admin
+
+
+
